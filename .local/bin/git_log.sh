861685e37ffd375f625e4052b9497ac83c5f89e1
William Brown <williambrown.nz@protonmail.com>
2025-12-23 15:43:08 +1300

    .gitconfig: Add custom log and st aliases

---
+++ .local/bin/git_log.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+LF="%C(auto)"
+
+# short hash
+LF+="%h"
+
+# subject
+LF+=" "
+LF+="%<(60,trunc)"
+LF+="%s"
+
+# author name
+LF+=" "
+LF+="%C(#666666)"
+LF+="%<(16,trunc)"
+LF+="%an"
+LF+="%C(auto)"
+
+# relative date
+LF+=" "
+LF+="%C(#569cd6)"
+LF+="%<(14,trunc)"
+LF+="%ar"
+LF+="%C(auto)"
+
+# ref names
+LF+=" "
+LF+="%D"
+
+git log --pretty=format:"$LF"
